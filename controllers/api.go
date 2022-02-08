62d2f9eef201d720dc17e8e674eb23c87b5a47bf
Kazuki Hasegawa <hasse09052@gmail.com>
2022-02-08 14:58:05 +0900

    透かしの埋め込み・解読をmodelに移動

--- controllers/api.go
+++ controllers/api.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"watermarkApi/lib"
 	"watermarkApi/models"
 
 	"github.com/gin-gonic/gin"
@@ -11,10 +10,8 @@ func Embed(c *gin.Context) {
 	var watermark models.Watermark
 	c.BindJSON(&watermark)
 
-	image := lib.DecodeBase64(watermark.Image)
-	embedImage := lib.EmbedWatermark(image, watermark.Text)
 	c.JSON(200, models.Watermark{
-		Image: lib.EncodeBase64(embedImage),
+		Image: watermark.EmbedWatermark(),
 		Text:  watermark.Text,
 	})
 }
@@ -23,9 +20,7 @@ func Decode(c *gin.Context) {
 	var watermark models.Watermark
 	c.BindJSON(&watermark)
 
-	image := lib.DecodeBase64(watermark.Image)
-	decodeText := lib.DecodeWatermark(image)
 	c.JSON(200, models.Watermark{
-		Text: decodeText,
+		Text: watermark.DecodeWatermark(),
 	})
 }
--- models/watermark.go
+++ models/watermark.go
@@ -1,6 +1,19 @@
 package models
 
+import "watermarkApi/lib"
+
 type Watermark struct {
 	Image string `json:"image"`
 	Text  string `json:"text"`
+}
+
+func (w Watermark) EmbedWatermark() string {
+	image := lib.DecodeBase64(w.Image)
+	embedImage := lib.EmbedWatermark(image, w.Text)
+	return lib.EncodeBase64(embedImage)
+}
+
+func (w Watermark) DecodeWatermark() string {
+	image := lib.DecodeBase64(w.Image)
+	return lib.DecodeWatermark(image)
 }
