f3089749072aea0870b259d81e8834f26394dd5d
Kazuki Hasegawa <hasse09052@gmail.com>
2022-02-08 10:46:01 +0900

    viewのファイルを組み込み

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"time"
+	"net/http"
 	"watermarkApi/lib"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -39,33 +38,7 @@ func decode(c *gin.Context) {
 func main() {
 	router := gin.Default()
 
-	router.Use(cors.New(cors.Config{
-		// アクセスを許可したいアクセス元
-		AllowOrigins: []string{
-			"https://localhost:5000",
-			"http://127.0.0.1:5500",
-		},
-		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
-		AllowMethods: []string{
-			"POST",
-			"GET",
-			"OPTIONS",
-		},
-		// 許可したいHTTPリクエストヘッダ
-		AllowHeaders: []string{
-			"Access-Control-Allow-Credentials",
-			"Access-Control-Allow-Headers",
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-			"Authorization",
-		},
-		// cookieなどの情報を必要とするかどうか
-		AllowCredentials: true,
-		// preflightリクエストの結果をキャッシュする時間
-		MaxAge: 24 * time.Hour,
-	}))
-
+	router.StaticFS("/", http.Dir("views"))
 	router.POST("/api/embed", embed)
 	router.POST("/api/decode", decode)
 
